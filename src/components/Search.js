5ceb379873337ae83c845c81026893a3f080d1f0
Iñigo Montánchez <mci.m89@gmail.com>
2024-11-08 23:02:59 +0100

    Initial commit

--- src/components/Search.js
+++ src/components/Search.js
@@ -7,11 +7,17 @@ const app = express();
 app.use(cors());
 const API_KEY = '912e645d47af5f8f8be768b203e58549e7b7d0cc'; // Replace with your actual API key
 const url = `https://www.giantbomb.com/api/game/?api_key=${API_KEY}&search=${searchTerm}`;
-
+const cors = `https://cors-anywhere.herokuapp.com/`;
 app.get('/api/data',   
  async (req, res) => {
   try {
-    const response = await axios.get(url,);
+    const response = await axios.get(cors + url, {
+      params: params,
+      headers: {
+          'origin':'x-requested-with',
+          "user-key": "40759257c55df398b0d23ca3f4da5579",
+          Accept: "application/json"
+      }});
     const results = response.data.results;
     onSearch(results);
   } catch (error) {
