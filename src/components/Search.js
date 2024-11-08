a7cc5fcfe6a4bde62201f3fcd82b4088203a828d
Iñigo Montánchez <mci.m89@gmail.com>
2024-11-08 22:52:13 +0100

    Initial commit

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,42 +1,24 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+const express = require('express');
+const cors = require('cors');
+const axios = require('axios');
 
+const app = express();
+
+app.use(cors());
 const API_KEY = '912e645d47af5f8f8be768b203e58549e7b7d0cc'; // Replace with your actual API key
+const url = `https://www.giantbomb.com/api/game/?api_key=${API_KEY}&search=${searchTerm}`;
 
-function Search({ onSearch }) {
-  const [searchTerm, setSearchTerm] = useState('');
+app.get('/api/data',   
+ async (req, res) => {
+  try {
+    const response = await axios.get(url,);
+    const results = response.data.results;
+    onSearch(results);
+  } catch (error) {
+    console.error(error);
+  }
+});
 
-  const handleInputChange = (event) => {
-    setSearchTerm(event.target.value);
-  };
-
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    if (!searchTerm) return;
-
-    const url = `https://www.giantbomb.com/api/game/?api_key=${API_KEY}&search=${searchTerm}`;
-    const cors = 'https://corsproxy.io/'
-
-    try {
-      const response = await axios.get(cors + url);
-      const results = response.data.results;
-      onSearch(results);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        placeholder="Search games"
-        value={searchTerm}
-        onChange={handleInputChange}
-      />
-      <button type="submit">Search</button>
-    </form>
-  );
-}
-
-export default Search;
+app.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
