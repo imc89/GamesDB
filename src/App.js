8461c75552f358ce10f75461d1a2bf0d98dda720
Iñigo Montánchez <mci.m89@gmail.com>
2024-11-08 21:58:58 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import React, { useState } from 'react';
 import './App.css';
-import Search from './components/Search';
 
 function App() {
-  const [searchResults, setSearchResults] = useState([]);
-
-  const handleSearch = (results) => {
-    setSearchResults(results);
-  };
-
   return (
     <div className="App">
       <h1>Game Search Library</h1>
-      <Search onSearch={handleSearch} />
-      {searchResults.length > 0 && (
-        <ul>
-          {searchResults.map((game) => (
-            <li key={game.name}>{game.name}</li>
-          ))}
-        </ul>
-      )}
+
     </div>
   );
 }
