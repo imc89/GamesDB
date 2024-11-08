43aa6bdae926c3deba02ccd7d919862969df2f5d
Iñigo Montánchez <mci.m89@gmail.com>
2024-11-08 22:13:50 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
+import React, { useState } from 'react';
 import './App.css';
+import Search from './components/Search';
 
 function App() {
+  const [searchResults, setSearchResults] = useState([]);
+
+  const handleSearch = (results) => {
+    setSearchResults(results);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Game Search Library</h1>
+      <Search onSearch={handleSearch} />
+      {searchResults.length > 0 && (
+        <ul>
+          {searchResults.map((game) => (
+            <li key={game.name}>{game.name}</li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
